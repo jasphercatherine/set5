df286933e728a133d9ac46f783c3c9c26b057e2c
jasphercatherine <47554944+jasphercatherine@users.noreply.github.com>
2019-06-25 20:17:58 +0530

    Update 41.py

--- 41.py
+++ 41.py
@@ -1,7 +1,4 @@
-n=int(input())
-x=list(map(int,input.split()))
-x=[1,2,3]
-st=" "
-for i in x:
-	st=st+st(i)+" "
-print(st.strip())
+string,vj =input().split()
+vj=int(vj)
+for i in range(vj):
+  print(string)
